13633de2ca4befe69d72a51a500570335520d91a
bmiao10 <brenda.miao@gmail.com>
2018-05-14 22:10:49 -0400

    Added additional views for recording and viewing

---
+++ ARQ/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  ARQ
+//
+//  Created by Brenda Miao on 5/14/18.
+//  Copyright © 2018 Brenda Miao. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
